013f0e676f3b04799bab77c8ca6f4b7f83c16d95
Maricar Walters <walterskai7@gmail.com>
2021-01-11 13:23:45 -0600

    initial commit for codingEvents practice for views

---
+++ CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+        static private List<string> Events = new List<string>();
+        // GET : /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            Events.Add("Strange Loop");
+            Events.Add("Women Who Code");
+            Events.Add("Diversify Tech");
+            ViewBag.events = Events;
+
+            return View();
+        }
+    }
+}
