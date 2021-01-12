3c506eddf6d64298ecd2cdff6bf0b5d030a993b6
Maricar Walters <walterskai7@gmail.com>
2021-01-12 14:27:18 -0600

    Created Event class and EventData class

--- CodingEvents/Controllers/EventsController.cs
+++ CodingEvents/Controllers/EventsController.cs
@@ -3,30 +3,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodingEvents.Models;
+
 
 namespace CodingEvents.Controllers
 {
     public class EventsController : Controller
     {
         //static private List<string> Events = new List<string>();
-        private static Dictionary<string, string> listOfEvents = new Dictionary<string, string>();
+        // private static Dictionary<string, string> listOfEvents = new Dictionary<string, string>();
+
+        static private List<Event> Events = new List<Event>();
 
         // GET : /<controller>/
         [HttpGet]
         public IActionResult Index()
         {
-            if (listOfEvents.ToList().Count == 0)
-            {
-                listOfEvents.Add("Strange Loop", "text");
-                listOfEvents.Add("Women Who Code", "text");
-                listOfEvents.Add("Diversify Tech", "text");
-
-            }
-            //Events.Add("Strange Loop");
+            //Events.Add("Strange Loop");       //for the old list
             //Events.Add("Women Who Code");
             //Events.Add("Diversify Tech");
 
-            ViewBag.listOfEvents =  listOfEvents;
+            //if (listOfEvents.ToList().Count == 0)         // optional auto populated events
+            //{
+            //    listOfEvents.Add("Strange Loop", "text");
+            //    listOfEvents.Add("Women Who Code", "text");
+            //    listOfEvents.Add("Diversify Tech", "text");
+            //}
+            //ViewBag.listOfEvents =  listOfEvents;
+
+            ViewBag.events = Events;
 
             return View();
         }
@@ -34,7 +39,6 @@ namespace CodingEvents.Controllers
         [HttpGet]
         public IActionResult Add()
         {
-            
             return View();
         }
 
@@ -42,7 +46,8 @@ namespace CodingEvents.Controllers
         [Route("/Events/Add")]
         public IActionResult NewEvent(string name, string description)
         {
-            listOfEvents.Add(name, description);
+            Events.Add(new Event(name, description));
+
             return Redirect("/Events");
         }
     }
